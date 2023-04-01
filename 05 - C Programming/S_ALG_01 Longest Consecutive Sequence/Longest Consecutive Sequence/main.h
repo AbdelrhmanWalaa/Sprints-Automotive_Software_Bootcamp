a6fd0442c45d9b6296b8830a7d9911f5aacad0fb
Abdelrhman Walaa <44446382+AbdelrhmanWalaa@users.noreply.github.com>
2023-04-01 05:33:22 +0200

    Add files via upload

---
+++ 05 - C Programming/S_ALG_01 Longest Consecutive Sequence/Longest Consecutive Sequence/main.h
@@ -0,0 +1,25 @@
+/*
+ * main.h
+ *
+ *   Created on: Apr 1, 2023
+ *       Author: Abdelrhman Walaa - https://github.com/AbdelrhmanWalaa
+ *  Description: This file contains all Main functions' prototypes and definitions (Macros) to avoid magic numbers.
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/**************************************************************************************************************************/
+/* Main Includes */
+#include "sort.h"
+#include "lcs.h"
+
+#pragma warning(disable : 4996)
+
+/**************************************************************************************************************************/
+/* Main Functions' Prototypes */
+void startProgram(void);
+
+/**************************************************************************************************************************/
+
+#endif /* MAIN_H_ */
