9614a14953208f20d62f1f928f96f5824ca8aee1
Abdelrhman Walaa <44446382+AbdelrhmanWalaa@users.noreply.github.com>
2023-04-12 13:47:22 +0200

    Add files via upload

---
+++ 06 - Embedded Systems L1/S_ESL1_02 LED sequence V2.0/LED Sequence V2.0/LED Sequence V2.0/Debug/main.d
@@ -0,0 +1,45 @@
+main.d main.o: .././main.c .././APP/APP_Interface.h \
+ .././APP/../LIB/STD_TYPES/STD_TYPES.h \
+ .././APP/../LIB/BIT_MATH/BIT_MATH.h \
+ .././APP/../MCAL/GLI_Driver/GLI_Interface.h \
+ .././APP/../MCAL/GLI_Driver/../../LIB/STD_TYPES/STD_TYPES.h \
+ .././APP/../MCAL/GLI_Driver/../../LIB/BIT_MATH/BIT_MATH.h \
+ .././APP/../MCAL/EXI_Driver/EXI_Interface.h \
+ .././APP/../MCAL/EXI_Driver/../../LIB/STD_TYPES/STD_TYPES.h \
+ .././APP/../MCAL/EXI_Driver/../../LIB/BIT_MATH/BIT_MATH.h \
+ .././APP/../HAL/LED_Driver/LED_Interface.h \
+ .././APP/../HAL/LED_Driver/../../LIB/STD_TYPES/STD_TYPES.h \
+ .././APP/../HAL/LED_Driver/../../LIB/BIT_MATH/BIT_MATH.h \
+ .././APP/../HAL/LED_Driver/../../MCAL/DIO_Driver/DIO_Interface.h \
+ .././APP/../HAL/LED_Driver/../../MCAL/DIO_Driver/../../LIB/STD_TYPES/STD_TYPES.h \
+ .././APP/../HAL/LED_Driver/../../MCAL/DIO_Driver/../../LIB/BIT_MATH/BIT_MATH.h
+
+.././APP/APP_Interface.h:
+
+.././APP/../LIB/STD_TYPES/STD_TYPES.h:
+
+.././APP/../LIB/BIT_MATH/BIT_MATH.h:
+
+.././APP/../MCAL/GLI_Driver/GLI_Interface.h:
+
+.././APP/../MCAL/GLI_Driver/../../LIB/STD_TYPES/STD_TYPES.h:
+
+.././APP/../MCAL/GLI_Driver/../../LIB/BIT_MATH/BIT_MATH.h:
+
+.././APP/../MCAL/EXI_Driver/EXI_Interface.h:
+
+.././APP/../MCAL/EXI_Driver/../../LIB/STD_TYPES/STD_TYPES.h:
+
+.././APP/../MCAL/EXI_Driver/../../LIB/BIT_MATH/BIT_MATH.h:
+
+.././APP/../HAL/LED_Driver/LED_Interface.h:
+
+.././APP/../HAL/LED_Driver/../../LIB/STD_TYPES/STD_TYPES.h:
+
+.././APP/../HAL/LED_Driver/../../LIB/BIT_MATH/BIT_MATH.h:
+
+.././APP/../HAL/LED_Driver/../../MCAL/DIO_Driver/DIO_Interface.h:
+
+.././APP/../HAL/LED_Driver/../../MCAL/DIO_Driver/../../LIB/STD_TYPES/STD_TYPES.h:
+
+.././APP/../HAL/LED_Driver/../../MCAL/DIO_Driver/../../LIB/BIT_MATH/BIT_MATH.h:
