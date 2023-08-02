5aeb12bc8af16ddf89c422b3e08ea2b08e7e08f2
Abdelrhman Walaa <44446382+AbdelrhmanWalaa@users.noreply.github.com>
2023-08-02 08:56:53 +0300

    Delete 10 - ARM/S_ARM_02 RGB LED Control V1.0 directory

--- 10 - ARM/S_ARM_02 RGB LED Control V1.0/Project - RGB LED Control V1.0/Objects/delay_program.d
+++
@@ -1,4 +0,0 @@
-.\objects\delay_program.o: delay_program.c
-.\objects\delay_program.o: delay_interface.h
-.\objects\delay_program.o: std_types.h
-.\objects\delay_program.o: bit_math.h
--- 10 - ARM/S_ARM_02 RGB LED Control V1.0/Project - RGB LED Control V1.0/Objects/gpio_linkconfig.d
+++
@@ -1,4 +0,0 @@
-.\objects\gpio_linkconfig.o: gpio_linkConfig.c
-.\objects\gpio_linkconfig.o: gpio_linkConfig.h
-.\objects\gpio_linkconfig.o: std_types.h
-.\objects\gpio_linkconfig.o: bit_math.h
