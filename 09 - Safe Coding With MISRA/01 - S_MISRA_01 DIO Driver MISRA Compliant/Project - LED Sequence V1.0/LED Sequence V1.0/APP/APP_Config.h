dfa8d0d36a97eb5fa1a9570c59d73aaa7d3f0ce3
Abdelrhman Walaa <44446382+AbdelrhmanWalaa@users.noreply.github.com>
2023-08-10 17:00:15 +0300

    Add files via upload

---
+++ 09 - Safe Coding With MISRA/01 - S_MISRA_01 DIO Driver MISRA Compliant/Project - LED Sequence V1.0/LED Sequence V1.0/APP/APP_Config.h
@@ -0,0 +1,29 @@
+/*
+ * APP_Config.h
+ *
+ *   Created on: Apr 10, 2023
+ *       Author: Abdelrhman Walaa - https://github.com/AbdelrhmanWalaa
+ *  Description: This file contains all Application (APP) pre-build configurations, through which user can configure before using the APP.
+ */ 
+
+#ifndef APP_CONFIG_H_
+#define APP_CONFIG_H_
+
+/*******************************************************************************************************************************************************************/
+/* APP Configurations */
+
+/* LEDs Sequence States */
+#define APP_U8_PRESS_0_SEQ			0xF0
+#define APP_U8_PRESS_1_SEQ			0xF1
+#define APP_U8_PRESS_2_SEQ			0xF3
+#define APP_U8_PRESS_3_SEQ			0xF7
+#define APP_U8_PRESS_4_SEQ			0xFF
+#define APP_U8_PRESS_5_SEQ			0xFE
+#define APP_U8_PRESS_6_SEQ			0xFC
+#define APP_U8_PRESS_7_SEQ			0xF8
+
+/* End of Configurations */
+
+/*******************************************************************************************************************************************************************/
+
+#endif /* APP_CONFIG_H_ */
---
+++ 09 - Safe Coding With MISRA/01 - S_MISRA_01 DIO Driver MISRA Compliant/Project - LED Sequence V1.0/LED Sequence V1.0/Debug/HAL/BTN_Driver/BTN_Program.d
@@ -0,0 +1,22 @@
+HAL/BTN_Driver/BTN_Program.d HAL/BTN_Driver/BTN_Program.o: \
+ ../HAL/BTN_Driver/BTN_Program.c ../HAL/BTN_Driver/BTN_Config.h \
+ ../HAL/BTN_Driver/BTN_Interface.h \
+ ../HAL/BTN_Driver/../../LIB/STD_TYPES/STD_TYPES.h \
+ ../HAL/BTN_Driver/../../LIB/BIT_MATH/BIT_MATH.h \
+ ../HAL/BTN_Driver/../../MCAL/DIO_Driver/DIO_Interface.h \
+ ../HAL/BTN_Driver/../../MCAL/DIO_Driver/../../LIB/STD_TYPES/STD_TYPES.h \
+ ../HAL/BTN_Driver/../../MCAL/DIO_Driver/../../LIB/BIT_MATH/BIT_MATH.h
+
+../HAL/BTN_Driver/BTN_Config.h:
+
+../HAL/BTN_Driver/BTN_Interface.h:
+
+../HAL/BTN_Driver/../../LIB/STD_TYPES/STD_TYPES.h:
+
+../HAL/BTN_Driver/../../LIB/BIT_MATH/BIT_MATH.h:
+
+../HAL/BTN_Driver/../../MCAL/DIO_Driver/DIO_Interface.h:
+
+../HAL/BTN_Driver/../../MCAL/DIO_Driver/../../LIB/STD_TYPES/STD_TYPES.h:
+
+../HAL/BTN_Driver/../../MCAL/DIO_Driver/../../LIB/BIT_MATH/BIT_MATH.h:
